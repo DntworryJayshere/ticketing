2ae316a34eb7129931fc4ffd37dedc0dace52fda
Jay Rodriguez <jared.m.rodriguez@gmail.com>
2021-05-19 16:47:33 -0500

    payment-create event publisher and listener proliferation throughout microservices

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -10,6 +10,9 @@ import {
 } from '@ticketing_jr/common';
 import { stripe } from '../stripe';
 import { Order } from '../models/order';
+import { Payment } from '../models/payment';
+import { PaymentCreatedPublisher } from '../events/publishers/payment-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -38,6 +41,19 @@ router.post(
 			currency: 'usd',
 			source: token,
 		});
+		const payment = Payment.build({
+			orderId,
+			stripeId: charge.id,
+		});
+		await payment.save();
+
+		new PaymentCreatedPublisher(natsWrapper.client).publish({
+			id: payment.id,
+			orderId: payment.orderId,
+			stripeId: payment.stripeId,
+		});
+
+		res.status(201).send({ id: payment.id });
 	}
 );
 
