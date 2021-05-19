ce26fb3194be4acf71b5926c57b4a910677023c8
Jay Rodriguez <jared.m.rodriguez@gmail.com>
2021-05-19 14:40:30 -0500

    changes to global signing for usage of id if provided & new payment route testing

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -21,6 +21,16 @@ router.post(
 		const { token, orderId } = req.body;
 
 		const order = await Order.findById(orderId);
+
+		if (!order) {
+			throw new NotFoundError();
+		}
+		if (order.userId !== req.currentUser!.id) {
+			throw new NotAuthorizedError();
+		}
+		if (order.status === OrderStatus.Cancelled) {
+			throw new BadRequestError('Cannot pay for an cancelled order');
+		}
 	}
 );
 
