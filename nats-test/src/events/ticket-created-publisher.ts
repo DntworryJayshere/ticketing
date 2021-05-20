2b43f862b5fd0ce76c8a8480bf2cbcd0da6714a0
Jay Rodriguez <jared.m.rodriguez@gmail.com>
2021-05-20 11:11:41 -0500

    remove of nats test

--- nats-test/src/events/ticket-created-publisher.ts
+++
@@ -1,5 +0,0 @@
-import { Publisher, TicketCreatedEvent, Subjects } from '@ticketing_jr/common';
-
-export class TicketCreatedPublisher extends Publisher<TicketCreatedEvent> {
-	readonly subject = Subjects.TicketCreated;
-}
