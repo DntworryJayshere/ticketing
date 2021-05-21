cb88d9ba83e5d45ea7476a2950ff122251ab0819
Jay Rodriguez <jared.m.rodriguez@gmail.com>
2021-05-21 08:42:21 -0500

    changes for client and for github actions auth workflow

--- client/pages/index.js
+++ client/pages/index.js
@@ -33,10 +33,8 @@ const LandingPage = ({ currentUser, tickets }) => {
 };
 
 LandingPage.getInitialProps = async (context, client, currentUser) => {
-	console.log('LANDING PAGE!');
 	const { data } = await client.get('/api/tickets');
 
 	return { tickets: data };
 };
-
 export default LandingPage;
